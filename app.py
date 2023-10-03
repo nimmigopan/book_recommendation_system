aba4ab6408bc205b4b11a5b8a4c6f8b507ee252e
Nimmi <nimmigopan@gmail.com>
2023-10-03 15:35:20 +0400

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import pickle
+import numpy as np
 
 popularity_df = pickle.load(open('popular_books.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_score = pickle.load(open('similarity_score.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -9,15 +13,35 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html',
-                           book_name = list(popularity_df['Book-Title'].values),
-                           author = list(popularity_df['Book-Author'].values),
+                           book_name=list(popularity_df['Book-Title'].values),
+                           author=list(popularity_df['Book-Author'].values),
                            image=list(popularity_df['Image-URL-M'].values),
                            publisher=list(popularity_df['Publisher'].values),
                            rating=list(popularity_df['mean.of.ratings'].values)
                            )
 @app.route('/recommend')
-def recommend_book():
+def recommend_ui():
     return render_template('recommend.html')
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:6]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-S'].values))
+
+        data.append(item)
+    print(data)
+    return render_template('recommend.html',data=data)
+
+
 
 
 if __name__ == '__main__':
